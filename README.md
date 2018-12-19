7fb3ac9e03e085806540c144e663da70aa53883f
shavit <shavit@users.noreply.github.com>
2018-12-19 04:35:01 -0500

    Add events

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Chatbot App
+# Epic Victory Royale
+
+> Chatbot app for live streamers
+
+![Epic Victory Royale](https://raw.githubusercontent.com/shavit/musical-broccoli/master/Epic_Victory_Royale.jpg)
--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 // documentation https://chaturbate.com/apps/docs/index.html
 // support apps@chaturbate.com
 // https://testbed.chaturbate.com
+//
+// cb.sendNotice(msg1, [to_user], [background], [foreground], [weight], [to_group])
+//
 
-console.log(cb);
-cb.log('It works');
+//
+// PSA
+//
+
+function sendNotice(username){
+  let msgBg = '#7851A9';
+  let msgColor = '#FFFFFF';
+  let msg1 = (username == null)
+    ? '🚨 Attention All Streamers and Fans! 🚨'
+    : '🚨 Attention ' + username + '! 🚨';
+  let msg2 = 'PewDiePie is in great danger and need your help, to wipe out T-Series for once and for all.';
+
+  cb.sendNotice(msg1);
+  cb.sendNotice(msg1, [to_user], msgBg, msgColor);
+}
+
+function scheduleNotice() {
+    sendNotice(null);
+    cb.setTimeout(scheduleNotice, 1000 * 60 * 10);
+}
+
+//
+//  Events
+//
+
+cb.onEnter(function(user) {
+  noticeUser(user['user']);
+});
+
+cb.setTimeout(scheduleNotice, 1000 * 60 * 10);
