6921b38eeba2a8d479479c13824a86eae2a4b94b
shavit <shavit@users.noreply.github.com>
2018-12-19 04:57:27 -0500

    Style all the messages

--- index.js
+++ index.js
@@ -9,16 +9,23 @@
 // PSA
 //
 
-function sendNotice(username){
+function sendNotice(user){
   let msgBg = '#7851A9';
   let msgColor = '#FFFFFF';
-  let msg1 = (username == null)
+  let msg1 = (user == null)
     ? '🚨 Attention All Streamers and Fans! 🚨'
-    : '🚨 Attention ' + username + '! 🚨';
-  let msg2 = 'PewDiePie is in great danger and need your help, to wipe out T-Series for once and for all.';
+    : '🚨 Attention ' + user['user'] + '! 🚨';
+  let messages = [
+    msg1,
+    'PewDiePie is in great danger and need your help, to wipe out T-Series for once and for all.',
+    'To help him, all PewDiePie needs is for you to subscribe to his channel - it\'s pretty easy.',
+    'Be sure to hit that bell and turn the notifications on, and spread the news with all your friends throughout the world. But you have to be quick, so PewDiePie can achieve the Epic Victory.',
+    'https://youtube.com/pewdiepie',
+  ];
 
-  cb.sendNotice(msg1);
-  cb.sendNotice(msg1, [to_user], msgBg, msgColor);
+  messages.map((msg) => {
+    cb.sendNotice(msg, user, msgBg, msgColor);
+  });
 }
 
 function scheduleNotice() {
@@ -31,7 +38,7 @@ function scheduleNotice() {
 //
 
 cb.onEnter(function(user) {
-  sendNotice(user['user']);
+  sendNotice(user);
 });
 
 cb.setTimeout(scheduleNotice, 1000);
